b62b1cdfccfba7e3fe55d19d2650d59f714e1761
BridgitKirika <kirikabetty@gmail.com>
2023-06-29 19:31:29 +0300

    initial commit

---
+++ src/components/Task.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Task = ({ task, onDelete, onToggle}) => {
+  return (
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} 
+    onDoubleClick={() => onToggle(task.id)}>
+      
+      <h3>{task.text}
+         <button style={{ color: 'red', cursor: 'pointer'}} 
+         onClick={() => onDelete(task.id)}>Delete</button> 
+         </h3>
+
+      <p>{task.day}</p>
+    </div>
+  )
+}
+
+export default Task
